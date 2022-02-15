2ef4f741126cc53e9bfc7112dc7115722b6a4f80
kennethyeong <jumpspx@sparrowops.com>
2022-02-15 11:34:11 +0800

    feat: division and multiplication feature done

--- go/money_test.go
+++ go/money_test.go
@@ -15,10 +15,53 @@ func TestMultiplication(t *testing.T) {
 	}
 }
 
+func TestMultiplicationInEuros(t *testing.T) {
+	tenEuros := Money{
+		amount:   10,
+		currency: "EUR",
+	}
+	twentyEuros := tenEuros.Times(2)
+
+	if twentyEuros.amount != 20 {
+		t.Errorf("Expect 20, got: [%+v]", twentyEuros.amount)
+	}
+}
+
+func TestDivision(t *testing.T) {
+	originalMoney := Money{amount: 4002, currency: "KRW"}
+	actualResult := originalMoney.Divide(4)
+	expectedResult := Money{amount: 1000.5, currency: "KRW"}
+
+	// if expectedMoneyAfterDivision != actualMoneyAfterDivision {
+	// 	t.Errorf("Expected %+v got %+v", expectedMoneyAfterDivision, actualMoneyAfterDivision)
+	// }
+
+	assertEqual(t, expectedResult, actualResult)
+}
+
 type Dollar struct {
 	amount int
 }
 
+type Money struct {
+	amount   float64
+	currency string
+}
+
 func (d Dollar) Times(multiplier int) Dollar {
 	return Dollar{d.amount * multiplier}
+}
+
+func (m Money) Times(multiplier int) Money {
+	return Money{amount: m.amount * float64(multiplier), currency: m.currency}
+}
+
+func (m Money) Divide(divisor int) Money {
+	return Money{amount: m.amount / float64(divisor), currency: m.currency}
+}
+
+func assertEqual(t *testing.T, expected Money, actual Money) {
+	if expected != actual {
+		t.Errorf("Expected %+v Got %+v", expected, actual)
+	}
 }
--- js/test_money.js
+++ js/test_money.js
@@ -14,3 +14,42 @@ let fiver = new Dollar(5);
 let tenner = fiver.times(2);
 
 assert.strictEqual(tenner.amount, 10);
+
+class Money {
+	constructor(amt, currency) {
+		this.amount = amt;
+		this.currency = currency;
+	}
+
+	// using arrow function somehow changes the return profile of new Money()
+	// divide = (divisor) => {
+	// 	return new Money(this.amount / divisor, this.currency);
+	// }
+	// times = (multiplier) => {
+	// 	return new Money(this.amount * multiplier, this.currency);
+	// }
+
+	divide(divisor) {
+		return new Money(this.amount / divisor, this.currency);
+	}
+
+	times(multiplier) {
+		return new Money(this.amount * multiplier, this.currency);
+	}
+}
+
+let tenEuros = new Money(10);
+let twentyEuros = tenEuros.times(2);
+
+assert.strictEqual(twentyEuros.amount, 20);
+
+let originalMoney = new Money(4002, "KRW");
+let expectedMoneyAfterDivision = new Money(1000.5, "KRW");
+
+assert.deepStrictEqual(originalMoney.divide(4), expectedMoneyAfterDivision);
+
+
+let fiveDollar = new Money(5, "USD")
+let tenDollar = new Money(10, "USD")
+
+assert.deepStrictEqual(fiveDollar.times(2), tenDollar)
